0a7fcc85ccccd8a9c64f7441c7134136fa14edee
Tomas Garcia <tomasqgarcia@gmail.com>
2022-08-22 08:09:43 -0300

    initial test

---
+++ README.md
@@ -0,0 +1,17 @@
+Run all tests
+
+```bash
+npx playwright test
+```
+
+Run specific test
+
+```bash
+npx playwright test login.spec.ts --headed
+```
+
+Reports
+
+```bash
+npx playwright show-report
+```
---
+++ e2e/login.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  // Go to https://staging.doomobr.com/admin/
+  await page.goto('https://staging.doomobr.com/admin/');
+
+  // Go to https://staging.doomobr.com/admin/login
+  await page.goto('https://staging.doomobr.com/admin/login');
+
+  // Click [placeholder="Email"]
+  await page.locator('[placeholder="Email"]').click();
+
+  // Click [placeholder="Email"]
+  await page.locator('[placeholder="Email"]').click();
+
+  // Fill [placeholder="Email"]
+  await page.locator('[placeholder="Email"]').fill('imendivil@doomobienesraices.com');
+
+  // Click [placeholder="Contraseña"]
+  await page.locator('[placeholder="Contraseña"]').click();
+
+  // Fill [placeholder="Contraseña"]
+  await page.locator('[placeholder="Contraseña"]').fill('Doomotja!9');
+
+  // Click button:has-text("Ingresá")
+  await page.locator('button:has-text("Ingresá")').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin');
+  await page.screenshot({ path: 'test-results/screenshot.png', fullPage: true });
+
+  // Click text=Pedidos
+  await page.locator('text=Pedidos').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/pedidos');
+
+  // Click text=Clientes
+  await page.locator('text=Clientes').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/clientes');
+
+  // Click text=Propiedades
+  await page.locator('text=Propiedades').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/propiedades');
+
+  // Click nav >> text=Publicaciones
+  await page.locator('nav >> text=Publicaciones').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/publicaciones');
+
+  // Click text=Agentes
+  await page.locator('text=Agentes').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/agentes');
+
+  // Click text=Cerrar Sesión
+  await page.locator('text=Cerrar Sesión').click();
+  await expect(page).toHaveURL('https://staging.doomobr.com/admin/login');
+
+});
