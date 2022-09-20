8652660495fb600797dcfb2d3357ab8d01570144
Tomas Garcia <tomasqgarcia@gmail.com>
2022-09-20 08:19:38 -0300

    update example

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+const BASE_URL = process.env.BASE_URL || "https://staging.doomobr.com"
+
 test('test', async ({ page }) => {
 
-  // Go to https://staging.doomobr.com/
-  await page.goto('https://staging.doomobr.com/');
+  // Go to`${BASE_URL}/
+  await page.goto(BASE_URL);
 
   // Click nav >> text=Publicaciones
   await page.locator('nav >> text=Publicaciones').click();
-  await expect(page).toHaveURL('https://staging.doomobr.com/publicaciones/BO');
+  await expect(page).toHaveURL(`${BASE_URL}/publicaciones/BO`);
 
   // Click #footer-menu >> text=Contacto
   await page.locator('#footer-menu >> text=Contacto').click();
-  await expect(page).toHaveURL('https://staging.doomobr.com/contacto');
+  await expect(page).toHaveURL(`${BASE_URL}/contacto`);
 
 });
