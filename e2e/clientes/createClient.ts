043c50528a3f33b2384dc587643a5a4dbe44fb86
Tomas Garcia <tomasqgarcia@gmail.com>
2022-10-12 08:57:14 -0300

    refactor create client

--- e2e/clientes/createClient.ts
+++ e2e/clientes/createClient.ts
@@ -9,38 +9,18 @@ async function createClient (page: Page, clientsPage: ClientsPage, clientsNavBar
   await expect(page).toHaveURL(clientsNavBarPage.url)
   await clientsPage.newClientButton.click()
 
-  // Fill [placeholder="Nombre\/s del cliente"]
-  await page.locator('[placeholder="Nombre\\/s del cliente"]').fill(client.firstname);
-
-  // Fill [placeholder="Apellido\/s del cliente"]
-  await page.locator('[placeholder="Apellido\\/s del cliente"]').fill(client.lastname);
-
-  // Fill [placeholder="Dirección de email"]
-  await page.locator('[placeholder="Dirección de email"]').fill(client.email);
-
-  // Fill [placeholder="Número de teléfono"]
-  await page.locator('[placeholder="Número de teléfono"]').fill(client.phone);
-
-  // Fill [placeholder="Fecha de cumpleaños"]
-  await page.locator('[placeholder="Fecha de cumpleaños"]').fill(client.birthday);
-
-  // Fill [placeholder="Número\/Dígitos del pasaporte"]
-  await page.locator('[placeholder="Número\\/Dígitos del pasaporte"]').fill(client.pasport);
-
-  // Select BO
-  await page.locator('select[name="passportCountryId"]').selectOption(client.pasportCountry);
-
-  // Fill [placeholder="Ingrese un nro\. de CI "]
-  await page.locator('[placeholder="Ingrese un nro\\. de CI "]').fill(client.identificationCard);
-
-  // Fill [placeholder="Ingrese un nro\. de NIT "]
-  await page.locator('[placeholder="Ingrese un nro\\. de NIT "]').fill(client.nit);
-
-  // Click text=Crear cliente
-  await page.locator('text=Crear cliente').click();
-
-  // Alert created client
-  await page.locator('div[role="alert"]:has-text("Éxito Cliente creado con éxito")').click();
+  await clientsPage.clientNameInput.fill(client.firstname);
+  await clientsPage.clientLastnameInput.fill(client.lastname);
+  await clientsPage.clientEmailInput.fill(client.email);
+  await clientsPage.clientPhoneInput.fill(client.phone);
+  await clientsPage.clientBirthdateInput.fill(client.birthday);
+  await clientsPage.clientPassportInput.fill(client.pasport);
+  await clientsPage.clientCountryInput.selectOption(client.pasportCountry);
+  await clientsPage.clientCIInput.fill(client.identificationCard);
+  await clientsPage.clientNITInput.fill(client.nit);
+  await clientsPage.createClientButton.click()
+  
+  await clientsPage.newClientAlert.click();
 };
 
 export default createClient
--- e2e/pages/clients-page.ts
+++ e2e/pages/clients-page.ts
@@ -1,15 +1,67 @@
 import { Page } from "@playwright/test"
 
-class ClientsPage{
+class ClientsPage {
     readonly page: Page
     // readonly url: string
 
-    constructor (page: Page) {
+    constructor(page: Page) {
         this.page = page
+    }
+
+    public get closeButton() {
+        return this.page.locator('[aria-label="Close"]')
     }
 
     public get newClientButton() {
         return this.page.locator('[data-test=create-client-button]')
+    }
+
+    public get createClientButton() {
+        return this.page.locator('text=Crear cliente')
+    }
+
+    public get editClientButton() {
+        return this.page.locator('text=Actualizar cliente')
+    }
+
+    public get newClientAlert() {
+        return this.page.locator('div[role="alert"]:has-text("Éxito Cliente creado con éxito")')
+    }
+
+    public get clientNameInput() {
+        return this.page.locator('[placeholder="Nombre\\/s del cliente"]')
+    }
+
+    public get clientLastnameInput() {
+        return this.page.locator('[placeholder="Apellido\\/s del cliente"]')
+    }
+
+    public get clientEmailInput() {
+        return this.page.locator('[placeholder="Dirección de email"]')
+    }
+
+    public get clientPhoneInput() {
+        return this.page.locator('[placeholder="Número de teléfono"]')
+    }
+
+    public get clientBirthdateInput() {
+        return this.page.locator('[placeholder="Fecha de cumpleaños"]')
+    }
+
+    public get clientPassportInput() {
+        return this.page.locator('[placeholder="Número\\/Dígitos del pasaporte"]')
+    }
+
+    public get clientCountryInput() {
+        return this.page.locator('select[name="passportCountryId"]')
+    }
+
+    public get clientCIInput() {
+        return this.page.locator('[placeholder="Ingrese un nro\\. de CI "]')
+    }
+
+    public get clientNITInput() {
+        return this.page.locator('[placeholder="Ingrese un nro\\. de NIT "]')
     }
 }
 
