edb1a7537d09fb1159ccaa595e58554a27b3f3b6
Tomas Garcia <tomasqgarcia@gmail.com>
2022-10-27 20:49:21 -0300

    validate agent from franchisee

---
+++ e2e/franchiseeValidateAgent.spec.ts
@@ -0,0 +1,29 @@
+import { test } from '@playwright/test';
+import login from './auth/login';
+import validateClient from './clientes/validateClient';
+import { editedClientExample as editedAgentClient } from './config/agentConsts';
+import AdminNavbarPage, { Pages } from './pages/admin-navbar-page';
+import ClientsPage from './pages/clients-page';
+
+export default function createTests() {
+  const BASE_URL = process.env.BASE_URL || "https://staging.doomobr.com"
+  test.beforeEach(async ({ page }) => {
+    const userEmail = process.env.EMAIL_FRANCHISEE
+    const userPassword = process.env.PASSWORD_FRANCHISEE
+    if (userEmail && userPassword)
+      await login(page, BASE_URL, userEmail, userPassword)
+    else { console.log('Invalid User Email'); return }
+
+    // test.use({
+    //   storageState: 'auth.json'
+    // });
+  })
+
+  test('franchisee flow', async ({ page }) => {
+    const clientsNavBarPage = new AdminNavbarPage(page, BASE_URL, Pages.Clientes)
+    const editedClientsPage = new ClientsPage(page, editedAgentClient)
+
+    // validate Agent client
+    await validateClient(clientsNavBarPage, editedClientsPage)
+  });
+}
