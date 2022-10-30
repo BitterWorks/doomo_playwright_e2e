7a2e5a8eb292fe025d383fb6b0ae590e48c58430
Tomas Garcia <tomasqgarcia@gmail.com>
2022-10-30 17:58:34 -0300

    fix create multiple clients

--- e2e/admin/agentCreateMultipleClients.spec.ts
+++ e2e/admin/agentCreateMultipleClients.spec.ts
@@ -1,38 +1,30 @@
-import { test } from '@playwright/test';
-import { clientExample, listingExample, propertyExample } from '../config/agentConsts';
+import { expect, test } from '@playwright/test';
+import { clientExample } from '../config/agentConsts';
 import { BASE_URL } from '../config/config';
 import login from './auth/login';
 import createClient from './clientes/createClient';
 import AdminNavbarPage, { Pages } from './pages/admin-navbar-page';
 import ClientsPage from './pages/clients-page';
-import ListingsPage from './pages/listings-page';
-import PropiertiesPage from './pages/properties-page';
-import createProperty from './propiedades/createProperty';
-import createListing from './publicaciones/createListing';
 
 
 export default function createTests() {
-    test.beforeEach(async ({ page }) => {
-      const userEmail = process.env.EMAIL_AGENT
-      const userPassword = process.env.PASSWORD_AGENT
-      if (userEmail && userPassword)
-        await login(page, BASE_URL, userEmail, userPassword)
-      else { console.log('Invalid User Email'); return }
+  test.setTimeout(800000)
+  test.use({
+    storageState: 'authAgent.json'
+  })
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${BASE_URL}/admin`);
+    await expect(page.locator('text=Doomo Admin')).toBeVisible({timeout:60000})
+  })
 
-      // test.use({
-      //   storageState: 'auth.json'
-      // });
-    })
-
-    test('agent create multiple clients', async ({ page }) => {
+  for (let i = 1; i <= 13; i++)
+    test(`agent create multiple clients ${i}`, async ({ page }) => {
+      
       const clientsPage = new ClientsPage(page, clientExample)
       const clientsNavBarPage = new AdminNavbarPage(page, BASE_URL, Pages.Clientes)
-
-      for (let i = 1; i <= 13; i++) {
-        console.log(`creating client n: ${i}`)
-        clientExample.email = Date.now().toString()
-        clientExample.phone = Date.now().toString()
-        await createClient(clientsPage, clientsNavBarPage)
-      }
-    });
+      
+      clientExample.email = Date.now().toString()
+      clientExample.phone = Date.now().toString()
+      await createClient(clientsPage, clientsNavBarPage)
+    })
 }
--- e2e/test.list.ts
+++ e2e/test.list.ts
@@ -2,30 +2,49 @@ import { test } from "@playwright/test"
 
 import editProfileAgent from './admin/editProfileAgent.spec'
 import editProfileFranchisee from './admin/editProfileFranchisee.spec'
+import agentLogin from './admin/agentLogin.spec'
 import agentFlow from './admin/agentFlow.spec'
 import agentDelete from './admin/agentDelete.spec'
 import franchiseeFlow from './admin/franchiseeFlow.spec'
 import franchiseeDelete from './admin/franchiseeDelete.spec'
+import franchisorFlow from './admin/franchisorFlow.spec'
 import franchiseeValidateAgent from './admin/franchiseeValidateAgent.spec'
 import franchisorValidateAgent from './admin/franchisorValidateAgent.spec'
 import franchisorValidateFranchisee from './admin/franchisorValidateFranchisee.spec'
 import agentCreateMultipleListings from './admin/agentCreateMultipleListings.spec'
+import agentCreateMultipleProperties from './admin/agentCreateMultipleProperties.spec'
+import agentCreateMultipleClients from './admin/agentCreateMultipleClients.spec'
 import home from './portal/home'
+
+// Login
+test.describe(agentLogin)
 
 // Admin
 // test.describe(editProfileAgent)
 // test.describe(editProfileFranchisee)
+
 test.describe(agentFlow)
 test.describe(franchiseeFlow)
-test.describe(agentCreateMultipleListings)
-test.describe(franchiseeValidateAgent)
-test.describe(franchisorValidateAgent)
-test.describe(franchisorValidateFranchisee)
+
+// NO VE NADA SUS PROPIOS CLIENTES
+// test.describe(franchisorFlow)
+
+// Roles
+// test.describe(franchiseeValidateAgent)
+
+// NO VE NADA DE LOS DEMAS
+// test.describe(franchisorValidateAgent)
+// test.describe(franchisorValidateFranchisee)
 
 // Portal
-test.describe(home)
+// test.describe(home)
 
 
 // Admin
-test.describe(agentDelete)
-test.describe(franchiseeDelete)
+// test.describe(agentDelete)
+// test.describe(franchiseeDelete)
+
+// Fixtures
+test.describe(agentCreateMultipleClients)
+// test.describe(agentCreateMultipleProperties)
+// test.describe(agentCreateMultipleListings)
